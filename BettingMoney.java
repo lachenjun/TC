adad212755de30a24258e03f610003957d5c33d9
lachenjun <lachenjun@lachenjun-ThinkPad-X200.(none)>
2013-06-04 22:49:20 +0800

    Add BettingMoney.java MatchMaking.java UserName.java VolumeGuess.java

---
+++ BettingMoney.java
@@ -0,0 +1,24 @@
+package SRM191.D2L1.V250;
+
+public class BettingMoney {
+
+	public static int moneyMade(int[] amounts, int[] centsPerDollar,
+			int finalResult) {
+		int gain = 0;
+		for (int i = 0; i < amounts.length; i++) {
+			if (i == finalResult)
+				gain -= amounts[i] * centsPerDollar[i];
+			else
+				gain += amounts[i] * 100;
+		}
+		return gain;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(moneyMade(
+				new int[] { 5000, 5000 }, 
+				new int[] { 100,2 }, 0));
+
+	}
+
+}
---
+++ VolumeGuess.java
@@ -0,0 +1,26 @@
+package SRM191.V250and500;
+
+import java.util.StringTokenizer;
+
+public class VolumeGuess {
+
+	public static int correctVolume(String[] queries, int numberOfBoxes,
+			int ithBox) {
+		int[] boxs = new int[numberOfBoxes];
+		for (int i = 0; i < queries.length; i++) {
+			StringTokenizer st = new StringTokenizer (queries[i], ",");
+			int box1 = Integer.parseInt(st.nextToken());
+			int box2 = Integer.parseInt(st.nextToken());
+			int smallValue = Integer.parseInt(st.nextToken());
+			boxs[box1-1] = Math.max(boxs[box1-1], smallValue);
+			boxs[box2-1] = Math.max(boxs[box2-1], smallValue);
+		}
+
+		return boxs[ithBox-1];
+	}
+
+	public static void main(String[] args) {
+		System.out.println(correctVolume(new String [] 	{"1,2,31", "1,3,9", "1,4,31", "2,4,32", "3,4,9", "3,2,9"}, 4, 1));
+
+	}
+}
